b6a35c4613676199eeea2e5855cb2dc396513c14
aumujun <aumujun@gmail.com>
2019-04-26 19:25:04 +0800

    add forget_password view functions

--- app/web/auth.py
+++ app/web/auth.py
@@ -2,17 +2,17 @@ from app.models.user import User
 from app.models.base import db
 from . import web
 from flask import render_template, request, redirect, url_for, make_response, flash
-from app.forms.auth import RegisterForm, LoginForm
+from app.forms.auth import RegisterForm, LoginForm, EmailForm
 from flask_login import login_user, logout_user
 
-__author__ = '七月'
+__author__ = '0x1un'
 
 
 @web.route('/register', methods=['POST', 'GET'])
 def register():
-    """
-    注册试图函数
-    :return:
+    """
+    注册试图函数
+    :return:
     """
     form = RegisterForm(request.form)
     if request.method == "POST" and form.validate():
@@ -31,7 +31,7 @@ def login():
     if request.method == "POST" and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user) # login use and set cookies into the browser
+            login_user(user)  # login use and set cookies into the browser
             next: str = request.args.get('next')
             if not next or not next.startswith('/'):
                 next: str = url_for('web.index')
@@ -42,10 +42,16 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-
 @web.route('/reset/password', methods=['GET', 'POST'])
 def forget_password_request():
-    pass
+    form = EmailForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            account_email = form.email.data
+            user = User.query.filter_by(
+                email=account_email).first_or_404()  # 自动处理异常
+
+    return render_template("auth/forget_password_request.html", form=form)
 
 
 @web.route('/reset/password/<token>', methods=['GET', 'POST'])
