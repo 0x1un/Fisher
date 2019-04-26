ed4613df3cf6ae1153bc3085e61882783c39571a
aumujun <aumujun@gmail.com>
2019-04-26 19:40:01 +0800

    add the page_not_found view functions, automatically handling 404 error to return 404.html pages

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,7 +2,7 @@
 __author__ = '0x1un'
 __date__ = '1/8/19 8:45 PM'
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 web = Blueprint('web', __name__)
 
@@ -10,3 +10,7 @@ from app.web import book, auth, drift, gift, main, wish
 
 # from app.web import user
 
+
+@web.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
