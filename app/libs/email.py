075f475f24f0e6e867cc13c8c24a1d5004f4d9ea
aumujun <aumujun@gmail.com>
2019-04-30 23:45:39 +0800

    send mail asynchronounsly

--- app/libs/email.py
+++ app/libs/email.py
@@ -1,7 +1,7 @@
 from app import mail
 from flask_mail import Message
-from flask import current_app, render_template
-
+from flask import current_app, render_template, app
+from threading import Thread
 
 def send_mail(to, subject, template, **kwargs):
     """
@@ -13,4 +13,19 @@ def send_mail(to, subject, template, **kwargs):
         sender=current_app.config['MAIL_USERNAME'],
         recipients=[to])
     msg.html = render_template(template, **kwargs)
-    mail.send(msg)
+    app = current_app._get_current_object()
+    threads = Thread(target=send_async_email, args=[app, msg])
+    threads.start()
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+
+        try:
+            mail.send(msg)
+        except Exception as e:
+            pass
+
+
+
+
