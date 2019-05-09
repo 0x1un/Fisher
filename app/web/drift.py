51a9518c1d137787ec677cdf7f3fac41e4ea21a9
aumujun <aumujun@gmail.com>
2019-05-09 19:10:44 +0800

    nothing

--- app/web/drift.py
+++ app/web/drift.py
@@ -1,6 +1,7 @@
 from . import web
-from flask_login import login_required, curent_user
-
+from flask_login import login_required, current_user
+from app.models.gift import Gift
+from flask import flash, render_template, redirect, url_for
 __author__ = '0x1un'
 
 
@@ -10,8 +11,21 @@ def send_drift(gid):
     """
     the users must to be login website.
     """
-    pass
+    current_gift = Gift.query.git_or_404(gid)
+    if current_gift.is_myself_books(current_gift.id):
+        flash("这本书是你自己的, 不能向自己索要书籍.")
+        return redirect(url_for('web.book_detail'), isbn=current_gift.isbn)
+    
+    can = current_user.can_send_drift()
+    if not can:
+        return render_template("not_enough_beans.html", beans=current_user.beans)
 
+
+    gifter = current_user.user.summary
+    return render_template("drift.html", gifter=gifter, user_beans=current_user.beans)
+
+
+    
     
 
 
